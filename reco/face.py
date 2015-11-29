1682dd61a8b3bef853eb85a0849b3dccf453dfd6
George Mathew <panzer@Georges-MacBook-Pro-2.local>
2015-11-29 14:48:38 -0500

    Refactored Path

--- reco/face.py
+++ reco/face.py
@@ -5,11 +5,9 @@ import numpy as np
 import cv2
 __author__ = 'panzer'
 
-OPEN_CV_PATH = '/Users/panzer/Softwares/miniconda/pkgs/opencv-2.4.8-np17py27_2/share/OpenCV/'
-HAAR_PATH = OPEN_CV_PATH + "/haarcascades/"
+HAAR_PATH = "haarcascades/"
 
-face_cascade = cv2.CascadeClassifier(HAAR_PATH+
-                                     'haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier(HAAR_PATH + 'haarcascade_frontalface_default.xml')
 img = cv2.imread('yalefaces_jpgs/subject01.jpg')
 
 
