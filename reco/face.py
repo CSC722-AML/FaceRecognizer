df643cc478df3d30d3f6bfadd223e689238c2f4d
George Mathew <panzer@Georges-MacBook-Pro-2.local>
2015-11-29 14:45:57 -0500

    XMLs

--- reco/face.py
+++ reco/face.py
@@ -5,14 +5,21 @@ import numpy as np
 import cv2
 __author__ = 'panzer'
 
-OPEN_CV_PATH = '/Users/george/Panzer/Softwares/miniconda/pkgs/opencv-2.4.8-np17py27_2/share/OpenCV/'
+OPEN_CV_PATH = '/Users/panzer/Softwares/miniconda/pkgs/opencv-2.4.8-np17py27_2/share/OpenCV/'
 HAAR_PATH = OPEN_CV_PATH + "/haarcascades/"
 
 face_cascade = cv2.CascadeClassifier(HAAR_PATH+
                                      'haarcascade_frontalface_default.xml')
 img = cv2.imread('yalefaces_jpgs/subject01.jpg')
 
+
+sift = cv2.SIFT()
 faces = face_cascade.detectMultiScale(img)
-print(faces)
+
+kp = sift.detect(img, None)
+
+img2 = cv2.drawKeypoints(img, kp)
+
+cv2.imwrite('sift_keypoints.jpg',img2)
 
 
